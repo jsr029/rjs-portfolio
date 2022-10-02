30b433250f7055f386d919ecde4a9561e6741795
jsr029 <jsr059@gmail.com>
2022-10-02 15:49:05 +0200

    Update

--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -1,16 +1,83 @@
 import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
+import { cv } from '../utils/presentation/cv'
 import { title } from './Home'
 
 function Cv() {
+  console.log(cv)
   return (
     <>
       <h1>{title + ', Cv'}</h1>
       <Header />
-      <div className='cv'>
-        <Footer />
-      </div>
+      {cv && cv.length > 0 && cv.map((c, i) => {
+        return (
+          <div className='cv' key={i}>
+            <div className="cv_header">
+              <div className="cv_header-location">
+                <ul>
+                  <li>{c.header.adresse}</li>
+                  <li>{c.header.lieu}</li>
+                  <li>{c.header.tel}</li>
+                </ul>
+              </div>
+              <div className="cv_header-contact">
+                <ul>
+                  <li>{c.contact.email}</li>
+                  <li>{c.contact.webSite}</li>
+                </ul>
+              </div>
+            </div>
+            <div className="cv_objectif">
+              <h2 className="cv_objectif-name">{c.objectif.name}</h2>
+              <div className="cv_objectif-bloc">
+                <div className='cv-objectif-title'><h2>{c.objectif.title}</h2></div>
+                <div className="cv_objectif-text">
+                  {c.objectif.text}
+                </div>
+              </div>
+            </div>
+            <div className='cv-skills'>
+              <h2>Comptétences</h2>
+              <div className="cv-skills-bloc">
+                {c.competences.map((cpt, ind) => {
+                  return (
+                    <div className="cv_skills" key={ind}>
+                      <div className="cv_skills-bloc">
+                        <h2>{cpt.subtitle}</h2>
+                        <ul>
+                          {cpt.items.map((cp, p) => {
+                            return (
+                              <li key={p}>{cp}</li>
+                            )
+                          })}
+                        </ul>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+            <div className="cv-xperiences">
+              <h2>Experiences Professionnelles</h2>
+              <div className="cv_xperiences-bloc">
+                <h2>Developpeur d'Applications</h2>
+                <p>Dunkerque</p>
+                <p>2016 - 2018</p>
+                <ul>
+                  <li>Analyser les besoins du client</li>
+                  <li>Réaliser des maquettes</li>
+                  <li>Concevoir et développer les programmes</li>
+                  <li>Analyser les problèmes techniques</li>
+                  <li>Déterminer les mesures correctives</li>
+                  <li>Réaliser les supports techniques</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+      <Footer />
     </>
   )
 }
---
+++ src/utils/presentation/cv.js
@@ -0,0 +1,88 @@
+export const cv = [
+    {
+        header: {
+            adresse: "13C rue Charles Gounod",
+            lieu: "29000 Quimper",
+            tel: "07.69.72.84.73"
+        },
+        contact: {
+            email: "jsr059@gmail.com",
+            webSite: "https://rjs-portfolio-tly2.vercel.app/"
+        },
+        objectif: {
+            title: "Objectif",
+            name: "JEAN-SEBASTIEN RAKOTONIRINA",
+            text: "Autodidacte passionné et motivé, issu d'un niveau BAC+2 en chimie et " +
+                "fraichement sorti d'une formation de Développeur d'Applications Front-End ReactJs, " +
+                "je suis à la recherche d'un poste de Déveeloppeur dans le domaine du web, idéalement 100% à distance."
+        },
+        competences: [
+            {
+                id: 1,
+                title: "Compétences",
+                subtitle: "Développeur Web",
+                items: [
+                    "Analyser les besoins du client",
+                    "Réaliser des maquettes",
+                    "Concevoir et développer les programmes",
+                    "Analyser les problèmes techniques",
+                    "Déterminer les mesures correctives",
+                    "Réaliser les supports techniques"
+                ]
+            },
+            {
+                id: 2,
+                title: "Compétences",
+                subtitle: "Commerce",
+                items: [
+                    "Organiser une unité de vente",
+                    "Agencer un espace de vente",
+                    "Valoriser un produit",
+                    "Suivre l'état des stocks",
+                    "Préparer les commandes",
+                    "Réceptionner un produit",
+                    "Vérifier la conformité de la livraison",
+                    "Stocker un produit"
+                ]
+            },
+            {
+                id: 3,
+                title: "Compétences",
+                subtitle: "Electromécanique",
+                items: [
+                    "Appliquer les procédures de test",
+                    "Sélectionner le matériel de contrôle ou d'essais et vérifier sa conformité",
+                    "Simuler les fonctionnalités d'un appareil",
+                    "Vérifier le fonctionnement d'un appareil",
+                    "Eprouver un appareil dans des conditions extrêmes"
+                ]
+            },
+            {
+                id: 4,
+                title: "Compétences",
+                subtitle: "Commerce",
+                items: [
+                    "Réaliser la mise en rayon",
+                    "Entretenir un espace de vente",
+                    "Réaliser le comptage des fonds de caisse",
+                    "Accueillir les personnes",
+                    "Proposer un service/produit adapté à la demande du client",
+                    "Contractualiser une vente",
+                    "Réaliser une gestion administrative"
+                ]
+            },
+            {
+                id: 5,
+                title: "Compétences",
+                subtitle: "Administrateur BDD",
+                items: [
+                    "Gérer l'interface administarteur de l'hébergeur (OVH, 1&1)",
+                    "Mettre en place la base via PhpMyAdmin (sql)",
+                    "Traiter l'information (collecter, classer et mettre à jour)",
+                    "Mettre en place les procédures techniques, d'utilisation et de sécurité des équipements informatiques",
+                    "Gérer les droits d'accès des utilisateurs"
+                ]
+            }
+        ]
+    }
+]
